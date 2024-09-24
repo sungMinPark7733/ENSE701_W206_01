b2c2ad804c49eeefc0effebe7a35044991d7d002
sungMinPark7733 <132970342+sungMinPark7733@users.noreply.github.com>
2024-09-24 21:37:03 +1200

    backend update

--- speed_backend/src/app.module.ts
+++ speed_backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './api/books/book.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.DB_URI),,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
