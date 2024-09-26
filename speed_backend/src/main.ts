8753c126bbccf4199d29aa1e02d99ad56aa35c48
sungMinPark7733 <132970342+sungMinPark7733@users.noreply.github.com>
2024-09-26 18:03:22 +1200

    db connection

--- speed_backend/src/main.ts
+++ speed_backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({ 
+    origin: true, 
+    credentials: true 
+  });
+
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
