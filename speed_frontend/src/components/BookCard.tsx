b629ea2d7b8f9e1054440aa89ab60bcefd1f890f
sungMinPark7733 <132970342+sungMinPark7733@users.noreply.github.com>
2024-09-28 10:54:43 +1200

    Reapply "book details format update"
    This reverts commit d647f4b135be59d4f2998d2bf27f0107bd58dc99.

--- speed_frontend/src/components/BookCard.tsx
+++ speed_frontend/src/components/BookCard.tsx
@@ -20,11 +20,6 @@ const BookCard = ({ book }: IProp) => {
 
   return (
     <div className='card-container' onClick={onClick}>
-      <img
-        src='https://images.unsplash.com/photo-1495446815901-a7297e633e8d'
-        alt='Books'
-        className='book-image'
-      />
       <div className='desc'>
         <h2>{book.title}</h2>
         <h3>{book.author}</h3>
