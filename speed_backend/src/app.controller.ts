4ed59081d54baa0a2ffb53518e329bfc5ee476c6
J0hnniee <set.johnny02@gmail.com>
2024-10-05 20:37:09 +1300

    Updated app.controller.ts

--- speed_backend/src/app.controller.ts
+++ speed_backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Get, Patch, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
