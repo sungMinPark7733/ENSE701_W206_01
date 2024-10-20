b37854a2d7767421e08b58246028b0eadac488f8
sungMinPark7733 <132970342+sungMinPark7733@users.noreply.github.com>
2024-10-20 22:14:50 +1300

    Update SortableTable.tsx
    specifying data type

--- speed_frontend/src/components/table/SortableTable.tsx
+++ speed_frontend/src/components/table/SortableTable.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 
-interface SortableTableProps {
-  headers: { key: string; label: string }[]; // Table headers
-  data: any[]; // Data for the table rows
-  onSort: (key: any) => void; // Pass sorting handler from parent component
-  sortConfig: { key: any; direction: string } | null; // Current sorting configuration
-  onRowClick?: (row: any) => void; // Optional row click handler for details view
+// Define a generic type for the data
+type DataItem = Record<string, string | number | boolean>;
+
+interface SortableTableProps<T extends DataItem> {
+  headers: { key: keyof T; label: string }[]; // Table headers
+  data: T[]; // Data for the table rows
+  onSort: (key: keyof T) => void; // Pass sorting handler from parent component
+  sortConfig: { key: keyof T; direction: 'ascending' | 'descending' } | null; // Current sorting configuration
+  onRowClick?: (row: T) => void; // Optional row click handler for details view
 }
 
-const SortableTable: React.FC<SortableTableProps> = ({
+const SortableTable = <T extends DataItem>({
   headers,
   data,
   onSort,
   sortConfig,
-  onRowClick, // Optional prop for row click
-}) => (
+  onRowClick,
+}: SortableTableProps<T>): React.ReactElement => (
   <table className="table-auto w-full">
     <thead>
       <tr>
         {headers.map((header) => (
           <th
-            key={header.key}
+            key={String(header.key)}
             onClick={() => onSort(header.key)}
             className="cursor-pointer"
           >
@@ -36,10 +39,10 @@ const SortableTable: React.FC<SortableTableProps> = ({
         <tr
           key={i}
           className="cursor-pointer hover:bg-gray-100"
-          onClick={() => onRowClick?.(row)} // Call the handler if provided
+          onClick={() => onRowClick?.(row)}
         >
           {headers.map((header) => (
-            <td key={header.key}>{row[header.key]}</td>
+            <td key={String(header.key)}>{row[header.key]}</td>
           ))}
         </tr>
       ))}
