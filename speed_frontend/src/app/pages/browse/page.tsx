e8ab0b23a22f58997d93686ca760d23be39311b0
sungMinPark7733 <132970342+sungMinPark7733@users.noreply.github.com>
2024-10-21 15:39:55 +1300

    browse page update
    connecting deployed backend to frontend

--- speed_frontend/src/app/pages/browse/page.tsx
+++ speed_frontend/src/app/pages/browse/page.tsx
@@ -43,7 +43,7 @@ const BrowsePage = () => {
   const fetchArticles = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:8082/articles");
+      const response = await fetch("https://ense-701-w206-01-wqfo.vercel.app/articles");
       const data = await response.json();
       setArticles(data);
     } catch (error) {
